4cab4377522dc429801cb470ddacef73a11b9397
unknownpgr <unknownpgr@gmail.com>
2020-07-13 03:16:51 +0900

    Adds an existing metadata creation script.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import React from 'react';
+// Import stylesheets
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Test from './Test.js';
+
+// Import metatdata
+import meta from './meta.json'
+
+// Import libraries
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  useLocation
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          I'll working on here.
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/unknownpgr"
-        >
-          Go to github
-        </a>
-      </header>
-      <Test></Test>
-    </div>
+    <Router>
+      <Navbar bg='dark' variant='dark' expand="lg">
+        <Navbar.Brand>This is a blog title!</Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Link to="/">Main</Link>
+            <Link to="/about">About</Link>
+          </Nav>
+        </Navbar.Collapse>
+
+      </Navbar>
+      <Switch>
+        <Route exact path="/">
+          <Main></Main>
+        </Route>
+        <Route path="/posts/:postName" component={Post}>
+        </Route>
+        <Route path="/about" component={About}>
+        </Route>
+        <Route component={NoMatch}>
+        </Route>
+      </Switch>
+    </Router>
   );
+}
+
+function NoMatch() {
+  return (
+    <div>Path {useLocation().pathname} is unregistered route.</div>
+  )
+}
+
+function Main() {
+  return (
+    <div>
+      TEST
+      <ul>
+        <h1>Post List</h1>
+        {meta.posts.map((post, i) =>
+          <li key={i}>
+            <Link to={post.postPath} >{post.title}</Link>
+          </li>
+        )}
+      </ul>
+      List Fin
+    </div>
+  )
+}
+
+function About(props) {
+  return <div>
+    This is my information!
+</div>
+}
+
+function Post(props) {
+  return (
+    <div>Selected post = {props.match.params.postName}</div>
+  )
 }
 
 export default App;
---
+++ src/posts/post1/test.md
@@ -0,0 +1,5 @@
+---
+title: This is test title
+category : This is test category
+---
+This is test file
