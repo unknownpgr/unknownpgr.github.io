56bedc217b1f144fad88c8938fcf61a7e9f83f25
unknownpgr <unknownpgr@gmail.com>
2020-07-14 04:38:05 +0900

    Adds sidebar and github logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // Import stylesheets
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/main.css";
+import img from "./img/github.png";
 
 // Import metatdata
 import { posts, postOrder, categories } from "./meta.json";
@@ -22,32 +23,58 @@ function App() {
   return (
     <Router>
       <div className="blog-title">
-        <h1 className="display-4 p-4 text-center text-light">{blogTitle}</h1>
+        <h1 className="display-4 py-4 text-center text-light">{blogTitle}</h1>
+        <a href="https://github.com/unknownpgr">
+          <img
+            src={img}
+            className="position-absolute rounded-circle"
+            id="blog-github"
+          ></img>
+        </a>
       </div>
-      <div className="container blog-container">
-        <Navbar bg="dark" variant="dark" expand="lg" className="rounded-top">
-          UnknownPgr의 블로그.
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link>
-                <Link to="/"> Main</Link>
-              </Nav.Link>
-              <Nav.Link>
-                <Link to="/about">About</Link>
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={MainPage}></Route>
-            <Route path="/posts/:postName" component={PostPage}></Route>
-            <Route path="/about" component={AboutPage}></Route>
-            <Route component={NoMatchPage}></Route>
-          </Switch>
+      <div id="blog-wrapper">
+        <div id="blog-side-left">LOL..</div>
+        <div id="blog-center">
+          <div className="container blog-container shadow">
+            <Navbar
+              bg="dark"
+              variant="dark"
+              expand="lg"
+              className="rounded-top"
+            >
+              UnknownPgr의 블로그.
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                  <Nav.Link>
+                    <Link to="/"> Main</Link>
+                  </Nav.Link>
+                  <Nav.Link>
+                    <Link to="/about">About</Link>
+                  </Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={MainPage}></Route>
+                <Route path="/posts/:postName" component={PostPage}></Route>
+                <Route path="/about" component={AboutPage}></Route>
+                <Route component={NoMatchPage}></Route>
+              </Switch>
+            </div>
+          </div>
+        </div>
+        <div id="blog-side-right">
+          This is right side bar with a lot of texts!
         </div>
       </div>
+      <footer class="page-footer font-small blue">
+        <div class="text-center py-4">
+          © 2020 Copyright :
+          <a href="https://github.com/unknownpgr"> Unknownpgr</a>
+        </div>
+      </footer>
     </Router>
   );
 }
@@ -58,7 +85,7 @@ function NoMatchPage() {
 
 function MainPage() {
   return (
-    <CardDeck>
+    <CardDeck className="pt-3 justify-content-around">
       {postOrder.map((post) => {
         let { postPath, title, category } = posts[post];
         console.log(posts[post]);
@@ -74,7 +101,10 @@ function AboutPage(props) {
 
 function Post(props) {
   return (
-    <Card className="text-white bg-dark" style={{ "max-width": "18rem" }}>
+    <Card
+      className="text-white bg-dark mb-3 shadow-sm"
+      style={{ "max-width": "24rem", "min-width": "16rem" }}
+    >
       <Card.Body>
         <Link to={props.href}>
           <Card.Title>{props.title}</Card.Title>
---
+++ src/posts/post1 copy/test.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+export default function(props) {
+    return (
+        <div className="blog-post">
+        <p>This is test file</p>
+<h1 id="andthisisheader">And this is header.</h1>
+<p>We can use <code>some code</code> like this.</p>
+        </div>
+    );
+};
