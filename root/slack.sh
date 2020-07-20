ba97fdd5d394d5f16120538b06bfc0dd0d6dbedf
unknownpgr <unknownpgr@gmail.com>
2020-07-21 03:19:24 +0900

    Auto commit at Tue Jul 21 03:19:24 KST 2020.

--- root/slack.sh
+++ root/slack.sh
@@ -1,1 +1,3 @@
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Github blog updated!"}' https://hooks.slack.com/services/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
+# To use this script, you set secrets SLACK_KEY.
+# For example : https://hooks.slack.com/services/T~~~~/~~~~
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Github blog updated!"}' ${{ secrets.SLACK_KEY }}
