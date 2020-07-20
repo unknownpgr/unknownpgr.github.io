0c89d1166f4e7909270696e3f20364c2f6ed30ad
unknownpgr <unknownpgr@gmail.com>
2020-07-21 04:15:10 +0900

    Update workflow

--- root/slack.sh
+++
@@ -1,3 +0,0 @@
-# To use this script, you set secrets SLACK_KEY.
-# For example : https://hooks.slack.com/services/T~~~~/~~~~
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Github blog updated!"}' "${{secrets.SLACK_KEY}}"
