a9935743a21c4495753968a8c6074d388220e128
unknownpgr <unknownpgr@gmail.com>
2020-08-28 16:53:05 +0900

    [update]Update auto publish scripts

--- blog_publish.bat
+++ blog_publish.bat
@@ -1,7 +1,7 @@
 git add .
-git commit -m "[post-update]Update post at %date% %time%."
+git commit -m "[update]Update post at %date% %time%."
 git pull origin master --no-commit
-git commit -m "[post-update]Marge remote at %date% %time%."
+git commit -m "[update]Marge remote at %date% %time%."
 git push origin master
 echo Publish finished.
 pause
