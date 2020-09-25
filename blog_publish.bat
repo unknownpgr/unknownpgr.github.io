6b600212d7a805ccbba70d22d99cc0e8a28cd3b4
Unknownpgr <unknownpgr@gmail.com>
2020-09-25 13:14:48 +0900

    Fix workflow not to run build without post update.

--- blog_publish.bat
+++ blog_publish.bat
@@ -1,7 +1,7 @@
 git add .
-git commit -m "[update]Update post at %date% %time%."
+git commit -m "Post update post at %date% %time%."
 git pull origin master --no-commit
-git commit -m "[update]Marge remote at %date% %time%."
+git commit -m "Post update and marge remote at %date% %time%."
 git push origin master
 echo Publish finished.
 pause
--- blog_publish.sh
+++ blog_publish.sh
@@ -1,6 +1,6 @@
 git pull origin master
 git add .
 if [[ `git status --porcelain` ]]; then
-	git commit -m "[update]Auto commit at $(date)."
+	git commit -m "Post update at $(date)."
 fi
 git push origin master
