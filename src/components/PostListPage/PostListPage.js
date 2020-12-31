7f37d2066deb2ec1cbcfdf1fd93bd5d3475e3ab8
unknownpgr <unknownpgr@gmail.com>
2020-12-31 16:40:09 +0900

    Optimize code

--- src/components/PostListPage/PostListPage.js
+++ src/components/PostListPage/PostListPage.js
@@ -3,24 +3,20 @@ import meta from "meta.json";
 import CardDeck from "react-bootstrap/CardDeck";
 import PostCard from "components/PostCard/PostCard";
 import "./postlistpage.scss";
+import mapDict from "libs/mapDict";
 
-class PostListPage extends React.Component {
-
-  render() {
-    return (
-      <CardDeck className="blog-post-list justify-content-around">
-        {Object.keys(meta)
-          .map(x => meta[x])
-          .map((post) => {
-            let { category } = post;
-            if (this.props.filter && this.props.filter !== category)
-              return null;
-            return <PostCard {...post} key={post.name}></PostCard>;
-          })
-          .filter((x) => x)}
-      </CardDeck>
-    );
-  }
+function PostListPage({ filter }) {
+  return (
+    <CardDeck className="blog-post-list justify-content-around">
+      {
+        mapDict(meta, (postName, post) => {
+          if (filter && filter !== post.category)
+            return null;
+          return <PostCard {...post} key={postName}></PostCard>;
+        }).filter((x) => x)
+      }
+    </CardDeck>
+  );
 }
 
 export default PostListPage;
