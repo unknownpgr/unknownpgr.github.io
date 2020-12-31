c22a2dcbe5ecfdcab87139b21484f5b0bd9aa0ea
unknownpgr <unknownpgr@gmail.com>
2020-12-31 19:05:08 +0900

    Update about page

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -42,14 +42,14 @@ function AboutPage(props) {
         title='깃허브 블로그'
         descriptions={[
           'Jekyll, Hexo 등 기존에 있는 프레임워크를 사용하지 않고 바닥에서부터 블로그를 만들어봤습니다.',
-          'React / SCSS 등 UI 프레임워크, GitHub Action을 사용한 CI/CD 등 웹 개발을 위한 기초 기술들을 많이 사용해본 프로젝트입니다.'
+          'React / SCSS 등 UI 프레임워크, GitHub Action을 사용한 CI/CD 등 웹 개발을 위한 기초 기술들을 많이 사용해봤습니다.'
         ]} />
       <ProjectItem
         link='https://github.com/unknownpgr/uos-urban'
         title='토공 다짐도 자동화시스템'
         descriptions={[
           '서울시립대학교 국제도시과학대학원에서 학생연구원으로서 진행한 프로젝트입니다.',
-          '현장에서 사용하는 장비에서 서버로 데이터 전송, 서버에서 DB에 데이터 저장 및 웹 페이지를 통한 시각화까지 구현한 프로젝트입니다.',
+          '현장에서 사용하는 장비에서 서버로 데이터 전송, 서버에서 DB에 데이터 저장 및 웹 페이지를 통한 시각화까지 구현했습니다.',
         ]} />
       <ProjectGroup>Embedded</ProjectGroup>
       <ProjectItem
@@ -103,6 +103,13 @@ function AboutPage(props) {
           '어떤 역학계의 운동에너지와 위치에너지로부터 라그랑지안을 계산하고, 각 일반화 좌표에 대해 푸는 MATLAB 스크립트입니다.',
           '추가로 JS 에서explicit Euler method를 사용하여 시뮬레이션을 만들어봤습니다.',
         ]} />
+      <ProjectItem
+        link='https://github.com/unknownpgr/fourier_transform'
+        title='이산 푸리에 변환'
+        descriptions={[
+          '주어진 곡선을 이산 푸리에 변환(DFT)을 이용하여 연속적인 곡선으로 변환해주는 C# 프로그램입니다.',
+          '지금 보기에는 조악한 구현이지만, 고등학생 때 구현했다는 것에 의의를 두겠습니다.',
+        ]} />
       <ProjectGroup>AI</ProjectGroup>
       <ProjectItem
         link='https://github.com/unknownpgr/face_recognize'
@@ -110,13 +117,25 @@ function AboutPage(props) {
         descriptions={[
           'OpenFace 라이브러리 및 SVM을 이용하여 얼굴 인식과 트래킹을 수행하도록 구현해봤습니다.',
         ]} />
-      <ProjectGroup>Fun projects</ProjectGroup>
+      <ProjectGroup label="인생은 즐겁게.">Fun projects</ProjectGroup>
       <ProjectItem
         link='https://github.com/unknownpgr/t-png'
         title='t-png'
         descriptions={[
           '배경색에 따라 서로 다른 이미지로 보이는 png이미지를 자동 생성하는 python/js 스크립트입니다.',
           'PseudoInverse 등 선형대수 연산을 사용했습니다.'
+        ]} />
+      <ProjectItem
+        link='https://github.com/unknownpgr/boolean-bruteforece'
+        title='Boolean Bruteforce'
+        descriptions={[
+          '주어진 truth-table을 만족시키는 가장 짧은 단일 boolean expression을 찾는 C++ 프로그램입니다.'
+        ]} />
+      <ProjectItem
+        link='https://github.com/unknownpgr/dragon-curve'
+        title='Dragon Curve'
+        descriptions={[
+          '프랙탈의 한 종류인 dragon curve를 그려봤습니다.'
         ]} />
     </ul>
     <h2>Work Experiences <Emoji emoji='👨‍💻' /></h2>
@@ -138,15 +157,6 @@ function AboutPage(props) {
     <h2>Honors / Awards <Emoji emoji='🏅' /></h2>
     <ul>
       <li>
-        <p>입학우수장학 (2019년 1학기)</p>
-      </li>
-      <li>
-        <p>국가장학 I (2019년 1학기~2020년 2학기)</p>
-      </li>
-      <li>
-        <p>학업우수장학(II) (2019년 2학기~2020년 2학기)</p>
-      </li>
-      <li>
         <p>서울시립대학교 2020년 X-TWICE 실전문제연구단 장려상</p>
         <ul>
           <li>딥러닝 기반 연관논문 추천시스템 개발 및 평가</li>
@@ -166,6 +176,15 @@ function AboutPage(props) {
       </li>
       <li>
         <p>서울시립대학교 2019 제22회 전국 라인트레이서 로봇 경연대회 장려상</p>
+      </li>
+      <li>
+        <p>입학우수장학 (2019년 1학기)</p>
+      </li>
+      <li>
+        <p>국가장학 I (2019년 1학기~2020년 2학기)</p>
+      </li>
+      <li>
+        <p>학업우수장학(II) (2019년 2학기~2020년 2학기)</p>
       </li>
     </ul>
     <h2>Publications <Emoji emoji='📔' /></h2>
