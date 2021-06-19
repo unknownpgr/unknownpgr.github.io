5e81fc05d6c65c05d4e11afa50d84fd715da7e7b
unknownpgr <unknownpgr@gmail.com>
2021-06-19 03:57:55 +0000

    Update frontend at Sat Jun 19 03:57:55 UTC 2021.

---
+++ precache-manifest.5a4c51f1d3d6ebda481fc8b2bf25bc15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0ce925f1da3f2a2a2614b7517620d15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "430c9a7dce08de9679d6",
+    "url": "/static/css/main.6b299993.chunk.css"
+  },
+  {
+    "revision": "9a3af93ca8de8198b0d0",
+    "url": "/static/js/2.5593e483.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.5593e483.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "430c9a7dce08de9679d6",
+    "url": "/static/js/main.f4a350f5.chunk.js"
+  },
+  {
+    "revision": "6b8ba8fc39846552c144",
+    "url": "/static/js/runtime-main.8d3cd085.js"
+  },
+  {
+    "revision": "b255767c409a2584a73d869662c911bb",
+    "url": "/static/media/nomatch.b255767c.jpg"
+  }
+]);
