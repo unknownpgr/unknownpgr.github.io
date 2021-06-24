c565d235c8b3b532f19a80071ad1a45897186e4d
unknownpgr <unknownpgr@gmail.com>
2021-06-25 01:27:26 +0900

    [build] Update workflow to use tag at 2021-06-25  1:27:26.05.

--- blog-build.bat
+++ blog-build.bat
@@ -2,9 +2,9 @@
 setlocal
 set /p msg=Enter commit message : 
 git add .
-git commit -m "[frontend]%msg% at %date% %time%."
+git commit -m "[build] %msg% at %date% %time%."
 git pull origin master --no-commit
-git commit -m "[frontend]%msg% and marge remote at %date% %time%."
+git commit -m "[build] %msg% and marge remote at %date% %time%."
 git push origin master
 echo Publish finished.
 pause
