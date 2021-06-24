c9e1f8a10e8c981f99762c63272fa6eb72c5696f
unknownpgr <unknownpgr@gmail.com>
2021-06-24 16:12:22 +0000

    Auto blog update at Thu Jun 24 16:12:22 UTC 2021

--- frontend/build/precache-manifest.2b473bddb95d90585b7bc99c4c604a6f.js
+++ frontend/build/precache-manifest.2b473bddb95d90585b7bc99c4c604a6f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b0ce925f1da3f2a2a2614b7517620d15",
+    "revision": "f51f36173cd31ae2efbc0b24c7fba257",
     "url": "/index.html"
   },
   {
-    "revision": "430c9a7dce08de9679d6",
+    "revision": "1fdc7ccf1551ca43d120",
     "url": "/static/css/main.6b299993.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.5593e483.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "430c9a7dce08de9679d6",
-    "url": "/static/js/main.f4a350f5.chunk.js"
+    "revision": "1fdc7ccf1551ca43d120",
+    "url": "/static/js/main.253d3eb8.chunk.js"
   },
   {
     "revision": "6b8ba8fc39846552c144",
