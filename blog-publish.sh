b3da6b8d6066fc37302d6a0fb8457da3144f3f27
UnknownPgr <unknownpgr@gmail.com>
2022-01-25 08:08:20 -0800

    Update publish script

--- blog-publish.sh
+++ blog-publish.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 cd "$(dirname "$0")"
+
+./blog-build.sh
+
 git add .
 git commit -m "[post] Post update post at $(date) $(time)."
-git pull origin master --no-commit
-git commit -m "[post] Post update and marge remote at $(date) $(time)."
 git push origin master
 echo Publish finished.
 read -p "Press any key to resume ..."
