8484b4b6e2777d99c922a6c571f6828be790f4bd
UnknownPgr <unknownpgr@gmail.com>
2022-01-25 07:45:55 -0800

    Add frontend / post build files

---
+++ docs/precache-manifest.6cef0950a08f39bdfc1caa1aa0ba2452.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be0b6a254436da4e1a761b355e41f802",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7a58a03cd53ba4bdc01",
+    "url": "/static/css/main.1440e35d.chunk.css"
+  },
+  {
+    "revision": "9a3af93ca8de8198b0d0",
+    "url": "/static/js/2.5593e483.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.5593e483.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7a58a03cd53ba4bdc01",
+    "url": "/static/js/main.050bafc6.chunk.js"
+  },
+  {
+    "revision": "6b8ba8fc39846552c144",
+    "url": "/static/js/runtime-main.8d3cd085.js"
+  },
+  {
+    "revision": "b255767c409a2584a73d869662c911bb",
+    "url": "/static/media/nomatch.b255767c.jpg"
+  }
+]);
