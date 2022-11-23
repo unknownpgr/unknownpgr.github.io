6e24e9bd33b37fef61bc0cd636986d31263cb113
unknownpgr <unknownpgr@gmail.com>
2022-11-23 05:15:02 +0000

    Add clear script

---
+++ clear.sh
@@ -0,0 +1,5 @@
+# Next.js related files
+rm -rf src/.next src/node_modules src/next-env.d.ts
+
+# Blog related files
+rm -rf src/.cache src/out src/public/imgs src/public/robots.txt src/public/sitemap*
