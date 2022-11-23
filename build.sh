809a0773f293b61595efb15dadf300d48b4c059b
unknownpgr <unknownpgr@gmail.com>
2022-11-23 05:40:48 +0000

    Implement post normalization

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 DIR="$(dirname -- $0)"
 cd $DIR
 docker build -t blog .
-docker run --rm -v $(pwd)/posts:/posts:ro -v $(pwd)/docs:/out blog
+docker run --rm -v $(pwd)/posts:/posts -v $(pwd)/docs:/out blog
