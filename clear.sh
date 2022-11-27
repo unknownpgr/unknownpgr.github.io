b697b7db40ec286422bb3094914e7cfd18ec6c82
unknownpgr <unknownpgr@gmail.com>
2022-11-27 11:16:12 +0000

    Update clear script

--- clear.sh
+++ clear.sh
@@ -1,5 +1,1 @@
-# Next.js related files
-rm -rf src/.next src/node_modules src/next-env.d.ts
-
-# Blog related files
-rm -rf src/.cache src/out src/public/imgs src/public/robots.txt src/public/sitemap*
+git clean -dx
