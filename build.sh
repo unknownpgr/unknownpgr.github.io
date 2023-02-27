6ad124221e2cd9e770da45149623765c5989b8ab
unknownpgr <unknownpgr@gmail.com>
2023-02-27 10:11:45 +0000

    Update build script

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
 # This script will build docker image for blog builder, and build blog from blog builder.
-DIR="$(dirname -- $0)"
-cd $DIR
+ROOT="$(dirname -- $0)"
+cd $ROOT
+
+git checkout build
+git rebase master
+
 docker build -t blog .
 docker run --rm -v $(pwd)/posts:/posts -v $(pwd)/docs:/out blog
+
+chown -R $(id -u):$(id -g) docs
+
+git add .
+git commit -m "Build blog"
+git checkout master
+git merge build --no-ff -m "Merge build branch"
+git push origin master
