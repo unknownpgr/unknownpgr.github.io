c9657ffa60386bc6fe3126b4a4194b43d937ec21
unknownpgr <unknownpgr@gmail.com>
2023-09-02 10:42:19 +0000

    Update tool page

--- src/frontend/src/pages/tools.tsx
+++ src/frontend/src/pages/tools.tsx
@@ -1,5 +1,21 @@
-import { useState } from "react";
+import { MouseEvent, useState } from "react";
 import styles from "./tools.module.css";
+
+function copyContentOnClick(e: MouseEvent<unknown, unknown>) {
+  const target = e.target;
+  if (!target) return;
+  const htmlTag = target as HTMLElement;
+  const text = htmlTag.textContent;
+  if (!text) return;
+
+  const tempInput = document.createElement("input");
+  tempInput.value = text;
+  document.body.appendChild(tempInput);
+  tempInput.select();
+  document.execCommand("copy");
+  document.body.removeChild(tempInput);
+  alert("Copied to clipboard");
+}
 
 function DCJGenerator() {
   const [registry, setRegistry] = useState("se.ction.link");
@@ -52,10 +68,20 @@ function DCJGenerator() {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
+          <label>Base64 Auth string</label>
+          <textarea
+            value={btoa(authString)}
+            onClick={copyContentOnClick}
+            readOnly
+          />
           <label>Dockerconfigjson</label>
-          <textarea value={dcjString} readOnly />
+          <textarea value={dcjString} onClick={copyContentOnClick} readOnly />
           <label>Secret</label>
-          <textarea value={secretString} readOnly />
+          <textarea
+            value={secretString}
+            onClick={copyContentOnClick}
+            readOnly
+          />
         </div>
       </p>
     </div>
