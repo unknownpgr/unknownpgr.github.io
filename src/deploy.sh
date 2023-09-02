db1ebe4807c911fe5427ee0b184c175237fdbcba
unknownpgr <unknownpgr@gmail.com>
2023-09-02 14:51:24 +0000

    Update build script

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,32 +1,13 @@
 cd `dirname $0`
 
 set -e
+shopt -s expand_aliases
+alias build='curl -sSL l.ist.sh/b | node - build'
 
 # Build images
 echo "Building images..."
-BACKEND_IMAGE=se.ction.link/blog-backend
-docker build -t $BACKEND_IMAGE:local backend 2> /dev/null
-FRONTEND_IMAGE=se.ction.link/blog-frontend
-docker build -t $FRONTEND_IMAGE:local frontend 2> /dev/null
-
-# Get image tags
-BACKEND_TAG=`echo $(docker inspect --format='{{.Id}}' $BACKEND_IMAGE:local) | md5sum | cut -c1-8`
-FRONTEND_TAG=`echo $(docker inspect --format='{{.Id}}' $FRONTEND_IMAGE:local) | md5sum | cut -c1-8`
-
-echo "Backend image tag: $BACKEND_TAG"
-echo "Frontend image tag: $FRONTEND_TAG"
-
-# Tag images
-docker tag $BACKEND_IMAGE:local $BACKEND_IMAGE:$BACKEND_TAG
-docker tag $FRONTEND_IMAGE:local $FRONTEND_IMAGE:$FRONTEND_TAG
-
-export BACKEND_IMAGE=$BACKEND_IMAGE:$BACKEND_TAG
-export FRONTEND_IMAGE=$FRONTEND_IMAGE:$FRONTEND_TAG
-
-# Push images
-echo "Pushing images..."
-docker push $BACKEND_IMAGE > /dev/null
-docker push $FRONTEND_IMAGE > /dev/null
+export BACKEND_IMAGE=`build backend se.ction.link/blog-backend`
+export FRONTEND_IMAGE=`build frontend se.ction.link/blog-frontend`
 
 # Deploy
 cat kubernetes/resources.yaml | envsubst | kubectl apply -f -
