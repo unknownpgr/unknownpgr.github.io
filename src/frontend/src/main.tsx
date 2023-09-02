2deaf52b19d5105d8ed7589341ca7ab73c5926aa
unknownpgr <unknownpgr@gmail.com>
2023-09-02 08:05:32 +0000

    Fix 100vh / mobile browser error

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -1,30 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements, useRouteError } from "react-router-dom"
-import App from './pages/main.tsx'
-import './index.css'
-import { Post } from "./pages/post.tsx"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  useRouteError,
+} from "react-router-dom";
+import App from "./pages/main.tsx";
+import "./index.css";
+import { Post } from "./pages/post.tsx";
 
 function ErrorBoundary() {
   const error = useRouteError();
   console.log(error);
-  const message = error instanceof Error ? error.message : "Nah"
-  return <div>
-    <h1>Something went wrong</h1>
-    <p>{message}</p>
-  </div>
+  const message = error instanceof Error ? error.message : "Nah";
+  return (
+    <div>
+      <h1>Something went wrong</h1>
+      <p>{message}</p>
+    </div>
+  );
 }
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    [<Route path="/" element={<App />} ErrorBoundary={ErrorBoundary} />,
-    <Route path="/posts/:id" element={<Post />} />
-    ]
-  )
+  createRoutesFromElements([
+    <Route path="/" element={<App />} ErrorBoundary={ErrorBoundary} />,
+    <Route path="/posts/:id" element={<Post />} />,
+  ])
 );
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = document.getElementById("root")!;
+function handleResize() {
+  if (!root) return;
+  root.style.height = `${window.innerHeight}px`;
+}
+window.addEventListener("resize", handleResize);
+handleResize();
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
